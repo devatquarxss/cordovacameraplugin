e6ac67dc0e5b1f2856819bd6d5b3b39c007bf937
priyanshuZoomtail <priyanshu@zoomtail.com>
2021-12-19 12:23:29 +0530

    fix for Android 11

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -303,16 +303,23 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
         intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
 
         if (this.cordova != null) {
-            // Let's check to make sure the camera is actually installed. (Legacy Nexus 7 code)
-            PackageManager mPm = this.cordova.getActivity().getPackageManager();
-            if(intent.resolveActivity(mPm) != null)
-            {
+            try {
                 this.cordova.startActivityForResult((CordovaPlugin) this, intent, (CAMERA + 1) * 16 + returnType + 1);
+            } catch(ActivityNotFoundException e) {
+                e.printStackTrace();
+                LOG.e(LOG_TAG, "Error: You don't have a default camera.  Your device may not be CTS complaint.");
             }
-            else
-            {
-                LOG.d(LOG_TAG, "Error: You don't have a default camera.  Your device may not be CTS complaint.");
-            }
+
+            // Let's check to make sure the camera is actually installed. (Legacy Nexus 7 code)
+            // PackageManager mPm = this.cordova.getActivity().getPackageManager();
+            // if(intent.resolveActivity(mPm) != null)
+            // {
+            //     this.cordova.startActivityForResult((CordovaPlugin) this, intent, (CAMERA + 1) * 16 + returnType + 1);
+            // }
+            // else
+            // {
+            //     LOG.d(LOG_TAG, "Error: You don't have a default camera.  Your device may not be CTS complaint.");
+            // }
         }
 //        else
 //            LOG.d(LOG_TAG, "ERROR: You must use the CordovaInterface for this to work correctly. Please implement it in your activity");
